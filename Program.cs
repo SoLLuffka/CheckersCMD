519273d49abf14d12dd18cb128dc2290a86c07f8
SoLLuffka <pl.kl0se.pl@gmail.com>
2024-12-10 20:47:11 +0100

    Simple player movement

--- Program.cs
+++ Program.cs
@@ -1,69 +1,180 @@
-﻿char [,] board = new char [8, 8]
+﻿internal class Program
 {
-    {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
-    {'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
-    {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
-    {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' ',},
-    {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'},
-    {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' ',}
-};
-
-char[] boardLetter = new char [8] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
-
-int player = 0;
-
-void showBoard(char[,] board)
-{
-    int size = board.GetLength(0);
-    Console.Write("    ");
-    for (int i = 0; i < boardLetter.GetLength(0); i++)
+    public static void Main(string[] args)
     {
-        Console.Write($" {boardLetter[i]}  ");
-    }
-    Console.WriteLine();
-    
-    Console.WriteLine("   +---+---+---+---+---+---+---+---+");
-
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($"  {i + 1} ");
-        for (int j = 0; j < size; j++)
+        char [,] board = new char [8, 8]
         {
-            Console.Write($" {board[i,j]} |");
-        }
-        Console.WriteLine();
-        Console.WriteLine("   +---+---+---+---+---+---+---+---+");
-    }
-}
+            {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
+            {'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
+            {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
+            {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' ',},
+            {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'},
+            {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' ',}
+        };
 
-bool isGameStill()
-{
-    char character, playerCharacter = ' ';
-    bool foundPlayer = false;
-    for (int i = 0; i < board.GetLength(0); i++)
-    {
-        for (int j = 0; j < board.GetLength(1); j++)
+        char[] boardLetter = new char [8] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+
+        bool player = true;
+
+        void showBoard(char[,] board)
         {
-            character = board[i,j];
-            if (character != ' ')
+            int size = board.GetLength(0);
+            Console.Write("    ");
+            for (int i = 0; i < boardLetter.GetLength(0); i++)
             {
-                if (!foundPlayer)
+                Console.Write($" {boardLetter[i]}  ");
+            }
+            Console.WriteLine();
+    
+            Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"  {i + 1} ");
+                for (int j = 0; j < size; j++)
                 {
-                    playerCharacter = character;
-                    foundPlayer = true;
-                } else if (character != playerCharacter)
+                    Console.Write($" {board[i,j]} |");
+                }
+                Console.WriteLine();
+                Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+            }
+        }
+
+        bool isGameStill()
+        {
+            char character, playerCharacter = ' ';
+            bool foundPlayer = false;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    return true;
+                    character = board[i,j];
+                    if (character != ' ')
+                    {
+                        if (!foundPlayer)
+                        {
+                            playerCharacter = character;
+                            foundPlayer = true;
+                        } else if (character != playerCharacter)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+    
+            return false;
+        }
+
+        void playerMove()
+        {
+            char playerChar = player ? 'O' : 'X';
+            Console.Write($"Player {playerChar} can now move! Write the cordinates of the pawn you want to move: ");
+            string pawnCordinate = Console.ReadLine();
+    
+            int pawnCordinateX = int.Parse(pawnCordinate.Substring(1)) - 1;
+            int pawnCordinateY = 0;
+            char pawnCordinateYchar = char.Parse(pawnCordinate.Substring(0, 1));
+
+            switch (pawnCordinateYchar)
+            {
+                case 'A':
+                    pawnCordinateY = 0;
+                    break;
+                case 'B':
+                    pawnCordinateY = 1;
+                    break;
+                case 'C':
+                    pawnCordinateY = 2;
+                    break;
+                case 'D':
+                    pawnCordinateY = 3;
+                    break;
+                case 'E':
+                    pawnCordinateY = 4;
+                    break;
+                case 'F':
+                    pawnCordinateY = 5;
+                    break;
+                case 'G':
+                    pawnCordinateY = 6;
+                    break;
+                case 'H':
+                    pawnCordinateY = 7;
+                    break;
+                default:
+                    break;
+            }
+            
+            // Console.WriteLine($"{board[pawnCordinateX, pawnCordinateY]}");
+
+            if (playerChar == 'O' && board[pawnCordinateX, pawnCordinateY] == 'O')
+            {
+                Console.Write("Write the cordinates of the field that you want to move: ");
+                string fieldCordinate = Console.ReadLine();
+                
+                int fieldCordinateX = int.Parse(fieldCordinate.Substring(1)) - 1;
+                int fieldCordinateY = 0;
+                char fieldCordinateYchar = char.Parse(fieldCordinate.Substring(0, 1));
+                
+                switch (fieldCordinateYchar)
+                {
+                    case 'A':
+                        fieldCordinateY = 0;
+                        break;
+                    case 'B':
+                        fieldCordinateY = 1;
+                        break;
+                    case 'C':
+                        fieldCordinateY = 2;
+                        break;
+                    case 'D':
+                        fieldCordinateY = 3;
+                        break;
+                    case 'E':
+                        fieldCordinateY = 4;
+                        break;
+                    case 'F':
+                        fieldCordinateY = 5;
+                        break;
+                    case 'G':
+                        fieldCordinateY = 6;
+                        break;
+                    case 'H':
+                        fieldCordinateY = 7;
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine($"Field: {fieldCordinateX}{fieldCordinateY} Pawn: {pawnCordinateX}{pawnCordinateY}");
+                Console.WriteLine();
+                if (true)
+                {
+                    if (board[fieldCordinateX, fieldCordinateY] == ' ' && pawnCordinateX - fieldCordinateX == 1 && (pawnCordinateY - fieldCordinateY == 1 || fieldCordinateY - pawnCordinateY == 1))
+                    {
+                        char pawn = board[pawnCordinateX, pawnCordinateY];
+                        board[fieldCordinateX, fieldCordinateY] = pawn;
+                        board[pawnCordinateX, pawnCordinateY] = ' ';
+                        pawn = ' ';
+                        // Console.Clear();
+                        player = true;
+                        showBoard(board);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error occured while moving field");
                 }
             }
         }
+
+        showBoard(board);
+        while (isGameStill())
+        {
+            playerMove();
+        }
     }
-    
-    return false;
 }
-
-
-
-showBoard(board);
--- obj/Debug/net8.0/Checkers CMD.AssemblyInfo.cs
+++ obj/Debug/net8.0/Checkers CMD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Checkers CMD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf198c50513933700dd0c5edc8f33078663babb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Checkers CMD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Checkers CMD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
